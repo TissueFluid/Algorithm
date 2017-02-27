4d09e1242a9299bbcbfaac48397847688f236de0
Zhiyue Zu <zuzhiyue@gmail.com>
2017-02-26 22:29:44 -0800

    Update Two Sum.cc

--- LeetCode/Two Sum.cc
+++ LeetCode/Two Sum.cc
@@ -1,23 +1,13 @@
-// Two sum
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
-        const int size = nums.size();
 
-        if (size < 2) {
-            return {};
-        }
-
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < nums.size(); ++i) {
             auto found = m.find(target - nums[i]);
 
-            if (found != m.cend()) {
-                return {found->second + 1, i + 1};
+            if (found != end(m)) {
+                return {found->second, i};
             }
 
             m[nums[i]] = i;
